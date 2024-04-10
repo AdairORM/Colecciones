e428bb92ffc31b8948127bd896d82fc17f4615ee
Alumno <Alumno@L16-2543304>
2024-04-10 12:02:56 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace Colecciones
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            ArrayList funkos = new ArrayList();
+
+            funkos.Add("Batman");
+            funkos.Add("Batman 80s");
+            funkos.Add("Superman");
+            funkos.Add(1938);
+            funkos.Add("Goyo");
+            funkos.Add(2000);
+
+            funkos.Remove("Superman");
+
+
+
+            /* for(int i = 0; i < funkos.Count; i++)
+             {
+                 //if (funkos[i].GetType() == typeof(Int32))
+
+                 Console.WriteLine("Funko: {0}",funkos[i]);
+             }
+            */
+            
+            funkos.Insert(2, "Flash");
+
+            foreach(var funko in funkos)
+            {
+                if (funko.GetType() == typeof(string))
+                Console.WriteLine(funko);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
